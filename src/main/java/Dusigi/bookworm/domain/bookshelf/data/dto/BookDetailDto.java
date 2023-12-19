353edd0ab7b4eb91dbab7835d9ed046a809b242f
오진서 <s22030@gsm.hs.kr>
2023-12-19 09:26:19 +0900

    create :: BookDetailDto

---
+++ src/main/java/Dusigi/bookworm/domain/bookshelf/data/dto/BookDetailDto.java
@@ -0,0 +1,18 @@
+package Dusigi.bookworm.domain.bookshelf.data.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDetailDto {
+    @NotBlank(message = "저자는 공백을 허용하지 않습니다.")
+    private String author;
+    @NotBlank(message = "제목은 공백을 허용하지 않습니다.")
+    private String title;
+    @NotBlank(message = "내용은 공백을 허용하지 않습니다.")
+    private String text;
+}
