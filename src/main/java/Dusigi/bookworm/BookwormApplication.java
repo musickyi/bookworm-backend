d40a08539f2200f3c4e12d1e7699291a18fb27d9
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-12-19 10:29:22 +0900

    feat :: update setting

--- src/main/java/Dusigi/bookworm/BookwormApplication.java
+++ src/main/java/Dusigi/bookworm/BookwormApplication.java
@@ -2,8 +2,12 @@ package Dusigi.bookworm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class BookwormApplication {
 
 	public static void main(String[] args) {
