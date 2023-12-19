42e2e4e20ec19e1b52c79ca962839b4f17bf06dd
오진서 <s22030@gsm.hs.kr>
2023-12-19 09:24:07 +0900

    delete :: Repository annotation

--- src/main/java/Dusigi/bookworm/domain/bookshelf/data/repository/BookRepository.java
+++ src/main/java/Dusigi/bookworm/domain/bookshelf/data/repository/BookRepository.java
@@ -2,11 +2,9 @@ package Dusigi.bookworm.domain.bookshelf.data.repository;
 
 import Dusigi.bookworm.domain.bookshelf.data.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findTitlesAndIdByBookshelf(String bookshelf);
+    List<Book> findBooksByBookshelf(String bookshelf);
 }
